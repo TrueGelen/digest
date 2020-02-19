83a155abefc7e8d388fe8a2b6f3f4901da174364
TrueGelen <54402826+TrueGelen@users.noreply.github.com>
2020-02-19 10:30:22 +0300

    desktop

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,5 +1,5 @@
-window.addEventListener('load', ()=>{
-  const buffer = null
+window.addEventListener('load', () => {
+  /* const buffer = null
   document.querySelector('body').addEventListener('click', ()=>{
     console.log(e.target)
     // if (e.target.classList.contain('burger') || e.target.parentNode.classList.contain('burger')){
@@ -10,5 +10,37 @@ window.addEventListener('load', ()=>{
         //аходим меню и добаляем класс showMenu
       }
     // }
+  }) */
+
+  const burgers = document.querySelectorAll('#burger')
+  const menu = document.querySelector('#menu')
+  const close = menu.querySelector('.menu__close')
+
+  function burgersController(toggle) {
+    if (toggle) {
+      for (let i = 0; i < burgers.length; i++)
+        burger[i].style.opacity = 1
+    } else {
+      for (let i = 0; i < burgers.length; i++)
+        burger[i].style.opacity = 0
+    }
+
+  }
+
+  for (let i = 0; i < burgers.length; i++) {
+    burgers[i].addEventListener('click', () => {
+      console.log(1)
+      menu.classList.remove('hideMenu')
+      menu.classList.add('showMenu')
+      burgersController(false)
+    })
+  }
+
+  close.addEventListener('click', () => {
+    console.log(2)
+    burgersController(true)
+    menu.classList.remove('showMenu')
+    menu.classList.add('hideMenu')
   })
+
 })
